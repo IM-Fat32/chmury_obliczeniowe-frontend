e006b4951e512c2401d4959b0fc9e6214dcd70f1
DawidNeumann <dawid.neumann.official@gmail.com>
2022-11-09 18:56:14 +0100

    first commit

---
+++ chmury_obliczeniowe/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["chm/obl/chmuryobliczeniowe/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ chmury_obliczeniowe/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"chmobl/chmury_obliczeniowe/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
